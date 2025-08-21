2cc70611f583350f3b7a060175c3e917408f0d99
Jeffreyfx1 <jeffreyudenze@gmail.com>
2025-08-21 09:12:48 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'JEPHTER TECH',
   botName: "☞ ᴊꜰx ᴍᴅ-xᴠ2",
   botOwner: '♛ᴊᴇᴘʜᴛᴇʀ-ᴛᴇᴄʜ', // Your name
-  ownerNumber: '2348146627582', // No + symbol
+  ownerNumber: '2349046157539', // No + symbol
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴡʜᴀᴛꜱᴀᴘᴘ ᴍᴅ ʙᴏᴛ ʙʏ ᴊᴇᴘʜᴛᴇʀ ᴛᴇᴄʜ. ᴄʜᴀɴɴᴇʟ: https://whatsapp.com/channel/0029VbAxkJl0lwgqAOojKI3R",
